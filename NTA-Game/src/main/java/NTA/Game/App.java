f9198b500e6ccc9d4ac5ee7b0df9a1b3b5eb94c9
Nicolas0694 <ndrisset@outlook.fr>
2020-04-30 10:51:34 +0200

    Réorganisation des modules

---
+++ NTA-Game/src/main/java/NTA/Game/App.java
@@ -0,0 +1,13 @@
+package NTA.Game;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
